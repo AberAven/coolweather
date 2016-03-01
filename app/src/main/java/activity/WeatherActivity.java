4745c9a983ff4f43d38ef41e4b4bb25a07af050e
HappyShare <782799011@qq.com>
2016-03-01 21:09:38 +0800

    添加自动更新天气 加入新图标

--- app/src/main/java/activity/WeatherActivity.java
+++ app/src/main/java/activity/WeatherActivity.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 
 import com.example.coolweather.R;
 
+import service.AutoUpdateService;
 import util.HttpCallbackListener;
 import util.HttpUtil;
 import util.Utility;
@@ -173,6 +174,8 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
         currentDateText.setText(prefs.getString("current_date", ""));
         weatherInfoLayout.setVisibility(View.VISIBLE);
         cityNameText.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
 
